be685a7c77074c6549b18b9701d312621362ca93
vprudenciano <101331486@georgebrown.ca>
2026-01-25 19:15:09 -0500

    add solution for week 02

---
+++ Week01/lab1.py
@@ -0,0 +1,14 @@
+# Sample Coding Questions 01 Week 01
+# Vienna Prudenciano
+
+numbers = [1, 4, 7, 9]
+
+a, b, c, d = 1, 2, 3, 4
+e = (a - ((b ** c) // d)) + (a % c)
+
+temperature = 32.6
+print("The temperature is: {:.3f} degrees celsius".format(temperature))
+
+userAge = input("Enter your age: ")
+userAge = int(userAge) + 22
+print ("Now showing the shop items filtered by age: {}".format(userAge))
